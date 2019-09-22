60b02c9e671dbe696eff1b772d194a5a6288cea2
Iran <irannlopes@gmail.com>
2019-09-22 15:53:59 -0300

    Commit inicial do Projeto com tela de splash e login

---
+++ app/src/main/java/com/example/aps_irh/Cat_Leitores.java
@@ -0,0 +1,14 @@
+package com.example.aps_irh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cat_Leitores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat__leitores);
+    }
+}
---
+++ app/src/main/java/com/example/aps_irh/Splash.java
@@ -0,0 +1,33 @@
+package com.example.aps_irh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.aps_irh.ui.login.LoginActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 4000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                Splash.this, LoginActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
